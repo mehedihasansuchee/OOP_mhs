93d4e27a569112303dde53a30da6a10f0eec6691
mehedihasanuchee <mehedihasansuchee@outlook.com>
2026-05-01 16:41:14 +0600

    class

---
+++ 01.Class/01.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, Id):
+        self.name = name
+        self.id = Id
+    
+s1 = Student("Bob", 1001)
+s2 = Student("Jon", 1100)
+
+print(s1.name, s1.id)
